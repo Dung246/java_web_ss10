fcdb978ac20f21bc5ced2677525ae09cc4482e74
Dung246 <dungoo2605@gmail.com>
2026-04-17 14:08:16 +0700

    Update TaskController.java

--- Java_web_Ss10_TH/src/main/java/org/example/project_base_spring_mvc/controller/TaskController.java
+++ Java_web_Ss10_TH/src/main/java/org/example/project_base_spring_mvc/controller/TaskController.java
@@ -1,21 +1,53 @@
 package org.example.project_base_spring_mvc.controller;
 
 
-import org.example.project_base_spring_mvc.model.TaskItem;
-import org.springframework.stereotype.Controller;
+package com.example.controller;
 
+import com.example.model.TaskItem;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class TaskController {
-    List<TaskItem> taskItems = new ArrayList<>();
-    public List<TaskItem> getTaskItems() {
 
-        return taskItems;
-    }
-    public void setTaskItems(List<TaskItem > taskItems){
-        this.taskItems=taskItems;
+    private List<TaskItem> tasks = new ArrayList<>();
+
+    public TaskController() {
+        tasks.add(new TaskItem("1", "Học Java cơ bản", LocalDate.now().plusDays(2), "HIGH"));
+        tasks.add(new TaskItem("2", "Làm bài tập Spring MVC", LocalDate.now().plusDays(3), "MEDIUM"));
     }
 
+    @GetMapping("/tasks")
+    public String getAll(Model model) {
+        model.addAttribute("tasks", tasks);
+        return "task-list";
+    }
+
+    @GetMapping("/tasks/add")
+    public String showForm(Model model) {
+        model.addAttribute("task", new TaskItem());
+        return "task-form";
+    }
+
+    @PostMapping("/tasks/add")
+    public String addTask(
+            @Valid @ModelAttribute("task") TaskItem task,
+            BindingResult result
+    ) {
+        if (result.hasErrors()) {
+            return "task-form";
+        }
+
+        task.setId(String.valueOf(tasks.size() + 1));
+        tasks.add(task);
+
+        return "redirect:/tasks";
+    }
 }
